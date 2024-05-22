ef0e0b3d92c7affe3a36dfbbb767243d75559eac
choiminkyung <choialsrud2002@gmail.com>
2024-05-22 21:04:51 +0900

    UserDefaluts 활용

--- SeSacApp0517/ViewController.swift
+++ SeSacApp0517/ViewController.swift
@@ -60,7 +60,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemGray2
         setUpLayout()
-        
+        saveData()
+//        let happy = UserDefaults.standard.string(forKey: "happy")
+//        happyLabel.text = happy
     }
     
     func buttonImageLayout(_ button : UIButton, imageName: String){
@@ -112,5 +114,71 @@ class ViewController: UIViewController {
         emotionCount[sender.tag] += 1
         
         countLabel[sender.tag].text = "\(emotionCount[sender.tag]) 번"
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        
+        UserDefaults.standard.set(happyLabel.text, forKey: "happy")
+        
+        UserDefaults.standard.set(loveLabel.text, forKey: "love")
+        
+        UserDefaults.standard.set(likeLabel.text, forKey: "like")
+        
+        UserDefaults.standard.set(panicLabel.text, forKey: "panic")
+        
+        UserDefaults.standard.set(upsetLabel.text, forKey: "upset")
+        
+        UserDefaults.standard.set(gloomyLabel.text, forKey: "gloomy")
+        
+        UserDefaults.standard.set(boredLabel.text, forKey: "bored")
+        
+        UserDefaults.standard.set(annoyingLabel.text, forKey: "annoying")
+        
+        UserDefaults.standard.set(sadLabel.text, forKey: "sad")
+        
+        print("터치")
+    }
+    
+    func saveData(){
+       
+        let happy = UserDefaults.standard.string(forKey: "happy")
+        happyLabel.text = happy
+        
+//        UserDefaults.standard.set(happyLabel.text, forKey: "happy")
+        
+        
+        let love = UserDefaults.standard.string(forKey: "love")
+        loveLabel.text = love
+//        UserDefaults.standard.set(loveLabel.text, forKey: "love")
+        
+        let like = UserDefaults.standard.string(forKey: "like")
+        likeLabel.text = like
+//        UserDefaults.standard.set(likeLabel.text, forKey: "like")
+        
+        let panic = UserDefaults.standard.string(forKey: "panic")
+        panicLabel.text = panic
+//        UserDefaults.standard.set(panicLabel.text, forKey: "panic")
+        
+        let upset = UserDefaults.standard.string(forKey: "upset")
+        upsetLabel.text = upset
+//        UserDefaults.standard.set(upsetLabel.text, forKey: "upset")
+        
+        let gloomy = UserDefaults.standard.string(forKey: "gloomy")
+        gloomyLabel.text = gloomy
+//        UserDefaults.standard.set(gloomyLabel.text, forKey: "gloomy")
+        
+        let bored = UserDefaults.standard.string(forKey: "bored")
+        boredLabel.text = bored
+//        UserDefaults.standard.set(boredLabel.text, forKey: "bored")
+        
+        let annoying = UserDefaults.standard.string(forKey: "annoying")
+        annoyingLabel.text = annoying
+//        UserDefaults.standard.set(annoyingLabel.text, forKey: "annoying")
+        
+        let sad = UserDefaults.standard.string(forKey: "sad")
+        sadLabel.text = sad
+//        UserDefaults.standard.set(sadLabel.text, forKey: "sad")
+        
+        
     }
 }
